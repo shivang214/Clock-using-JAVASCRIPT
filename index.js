490a840bb774faef074df4aa8a68522f44116be9
SHIVANG <shivang214@gmail.com>
2021-07-06 01:43:01 +0530

    first initialization

---
+++ index.js
@@ -0,0 +1,267 @@
+// JAVASCRIPT API
+// console.log("HELLO WORLD" + " KAY HAAL");
+// alert("ME");
+// console.log("HI");
+// document.write("this is doc write")
+// console.warn("warning")
+// console.error("THIS IS ERROR")
+// console.clear()
+
+// var n = 34
+// var m = 14
+// console.log(n + m);
+
+
+// var x = 14
+// var j = 56
+// console.log(x - (-j))
+
+
+// var str1 = "STRING 1";
+// var str2 = 'string 2';
+// console.log(str1)
+// console.log(str2)
+// console.log(str2 + " " + str1)
+
+
+// var marks = {
+//     h1: 45,
+//     h3: 54
+
+// }
+// console.log(marks)
+
+// var a = true
+// var b = false
+// console.log(a, b);
+
+
+// console.log("HI")
+// var x = 24
+// var y = 56
+// console.log(x + y)
+// console.log(x - y)
+
+// var n = null;
+// console.log(n)
+
+
+// var m = undefined;
+// console.log(m);
+
+// var arr = [1, 5, 4, 2, "SHIVANG", 5, 5, 6, 3]
+
+
+// assignment operators
+// var c = 12;
+// var b = c;
+// console.log(b);
+// b *= c
+// console.log(b);
+
+
+// var x = 41
+// var y = 45
+// console.log(x == y)
+// console.log(x > y)
+// console.log(x <= y)
+
+// console.log(true || false)
+// console.log(true && false)
+
+
+// console.log(!false)
+
+// console.log("HI")
+
+// function avg(a, b) {
+
+//     return (a + b) / 2;
+// }
+
+// c1 = avg(4, 6);
+// c2 = avg(5, 4);
+// console.log(c1, c2);
+
+
+// function x(m, s) {
+//     x = m * 5 + s * 4 + 55;
+//     return x;
+
+// }
+// o = x(1, 2);
+// console.log(o);
+
+// function age(x) {
+//     if (x >= 18)
+//         return true
+
+// }
+
+// v = age(19);
+// if (v == true) {
+//     console.log("VOTE DEDO");
+// }
+
+// var x = 41;
+// if (x > 222) {
+//     console.log("AGED")
+// }
+// else {
+//     console.log("YOUNG")
+// }
+
+
+// var x = 13;
+// if (x > 35) {
+//     console.log("NOT A KID")
+// }
+// else if (x > 25) {
+//     console.log("NOT A TEEN")
+// }
+// else {
+//     console.log("OLD")
+// }
+
+
+// var x = 45;
+// if (x > 90) {
+//     console.log("NO")
+// }
+// else if (x < 10) {
+//     console.log("OK")
+// }
+// else {
+//     console.log("HI")
+// }
+
+a = [1, 2, 3, 4, 56]
+// console.log(a.length)
+// for (var i = 0; i < a.length; i++) {
+//     console.log(a[i])
+// }
+
+
+// a.forEach(function (element) {
+//     console.log(element)
+// });
+
+// let j = 0;
+// while (j < a.length) {
+//     console.log(a[j])
+//     j++;
+// }
+// console.log("PRINT HELLO")
+
+
+// var x = 3;
+// while (x != 0) {
+//     console.log(x);
+//     x--;
+// }
+
+// if (x % 2 == 0) {
+//     console.log("EVEN")
+// }
+// else {
+//     console.log("ODD HA")
+// }
+
+
+// for (var i = 0; i < a.length; i++) {
+//     if (i == 2)
+//         continue;
+//     console.log(i)
+// }
+
+
+
+// var x = 4;
+// var y = 56;
+// console.log(x + y);
+
+// let arr = ["hi", 'hlo', 5, true, 56]
+// console.log(arr.length)
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// arr.push("SHIVANG")
+// arr.unshift()
+// console.log(arr)
+// console.log(arr.slice(0, 2))
+
+
+// let m = "i am bad boy";
+// d = m.replace("bad", "good");
+// console.log(d)
+
+
+
+// DATES :
+
+// let mydate = new Date();
+// console.log(mydate.getFullYear())
+
+
+// DOM MANIPULATION
+
+// let elem = document.getElementById("click")
+// console.log(elem)
+// elem.style.border = "4px solid blue"
+// console.log(elem.innerText)
+// console.log(elem.innerHTML)
+
+
+// tn = document.getElementsByTagName('div')
+// console.log(tn)
+// CreatedElement = document.createElement('p')
+// CreatedElement.innerText = "HELLO WORLD"
+// tn[0].appendChild(CreatedElement)
+
+
+// sel = document.querySelectorAll('.container')
+
+// console.log(sel)
+
+
+// window.onload = function hi() {
+//     console.log("SO JA BETE")
+// }
+
+
+// firstContainer.addEventListerner()
+
+
+
+// arrow functn
+
+// summ = (a, b) => {
+//     return a + b;
+// }
+
+// logkaro = () => {
+//     console.log("HELLO BETAJI")
+// }
+
+
+// setTimeout(logkaro, 2000);
+
+// localStorage.setItem("name", "shivang")
+// // localStorage
+// localStorage.getItem("name")
+// localStorage.clear();
+// localStorage.removeItem("name");
+
+
+
+// INTRO TO JSON
+ob = { name: "harry", length: 1, a: { this: "ThAT" } }
+obj = JSON.stringify(ob)
+console.log(typeof obj)
+console.log(obj)
+
+
+
+
+
+
